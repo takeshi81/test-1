b8abe9e273cc88c2131457cb82828897a612d2b8
REiJi <shino327gh@gmail.com>
2019-12-30 23:32:14 +0900

    ワインFRONT撮影画面の修正

--- syummy/js/size-adjust.js
+++ syummy/js/size-adjust.js
@@ -1,13 +1,16 @@
 $(function(){
    
     var wH = $(window).height();
-    wH *= 4/5;
+    wH *=4/5;
+  
     $('.bottle-saport').css('height',wH+'px');
     $('#camera').css('height',wH+'px');
+   
     $(window).on('resize',function(){
         var wH = $(window).height();
         wH *= 4/5;
         $('.bottle-saport').css('height',wH+'px');
         $('#camera').css('height',wH+'px');
+      
     });
 })
