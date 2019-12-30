ad92633d2fbabf3c56e80d6d16115a27edaa5049
REiJi <shino327gh@gmail.com>
2019-12-31 04:06:46 +0900

    全体撮影画面の修正
    カメラの画面が画面の８割で表示されるようにした。

--- syummy/js/size-adjust.js
+++ syummy/js/size-adjust.js
@@ -1,25 +1,29 @@
 $(function(){
    
     var wH = $(window).height();
+    var wHCameraCover = wH * 4/5;
     var wHNavFrontBack = wH * 225/1000;
     var wHWineBack = wH * 11/20;
     $('body').css('height',wH+ 'px');
     $('.navbar').css('height',wHNavFrontBack +'px');
     $('.bottle-saport').css('height',wHWineBack +'px');
-   
+    $('.bottle-saport-cover').css('height',wHCameraCover +'px');
     $('.pic-camera').css('height',wHWineBack +'px');
-    $('.wine-cover-img').css('height',wH+'px');
+    $('.pic-camera-cover').css('height',wHCameraCover +'px');
+   []
   
     $(window).on('resize',function(){
         var wH = $(window).height();
+        var wHCameraCover = wH * 5/6;
         var wHNavFrontBack = wH * 225/1000;
-    var wHWineBack = wH * 11/20;
+        var wHWineBack = wH * 11/20;
         $('body').css('height',wH+ 'px');
         $('.navbar').css('height',wHNavFrontBack +'px');
         $('.bottle-saport').css('height',wHWineBack +'px');
-        
+        $('.bottle-saport-cover').css('height',wHCameraCover +'px');
         $('.pic-camera').css('height',wHWineBack +'px');
-        $('.wine-cover-img').css('height',wH+'px');
+        $('.pic-camera-cover').css('height',wHCameraCover +'px');
+     
       
     });
 })
