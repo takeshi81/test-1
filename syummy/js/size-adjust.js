9db6fc49ca0b12f4efd811c3fbaba0264bfb01ab
REiJi <shino327gh@gmail.com>
2019-12-31 06:14:27 +0900

    ユーザー側top画面と店舗側top画面の修正

--- syummy/js/size-adjust.js
+++ syummy/js/size-adjust.js
@@ -4,26 +4,42 @@ $(function(){
     var wHCameraCover = wH * 4/5;
     var wHNavFrontBack = wH * 225/1000;
     var wHWineBack = wH * 11/20;
+    var wHBtnTop = wH*1/10;
+
+
+
     $('body').css('height',wH+ 'px');
+    //top画面のボタン
+    $('.btn-top').css('height',wHBtnTop +'px');
+    $('.top-title-text').css('margin-top',wH*1/10 +'px');
+    //撮影画面
     $('.navbar').css('height',wHNavFrontBack +'px');
     $('.bottle-saport').css('height',wHWineBack +'px');
     $('.bottle-saport-cover').css('height',wHCameraCover +'px');
     $('.pic-camera').css('height',wHWineBack +'px');
     $('.pic-camera-cover').css('height',wHCameraCover +'px');
-   []
+
   
     $(window).on('resize',function(){
         var wH = $(window).height();
-        var wHCameraCover = wH * 5/6;
+        var wHCameraCover = wH * 4/5;
         var wHNavFrontBack = wH * 225/1000;
         var wHWineBack = wH * 11/20;
+        var wHBtnTop = wH*1/10;
+    
+    
+    
         $('body').css('height',wH+ 'px');
+        //top画面のボタン
+        $('.btn-top').css('height',wHBtnTop +'px');
+        $('.top-title-text').css('margin-top',wH*1/10 +'px');
+        //撮影画面
         $('.navbar').css('height',wHNavFrontBack +'px');
         $('.bottle-saport').css('height',wHWineBack +'px');
         $('.bottle-saport-cover').css('height',wHCameraCover +'px');
         $('.pic-camera').css('height',wHWineBack +'px');
         $('.pic-camera-cover').css('height',wHCameraCover +'px');
-     
+    
       
     });
 })
